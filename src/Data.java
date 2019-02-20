7aa872f59189ea96170cc0d53b6648c644df9524
joao <joao182cx@gmail.com>
2019-02-20 10:13:07 -0300

    Criação da classe Data, que irá manipular a data inserida pelo usuário.

---
+++ src/Data.java
@@ -0,0 +1,76 @@
+
+public class Data {
+	
+	private int dia;
+	private int mes;
+	private int ano;
+	
+	public Data(int[] data) {
+		
+		mes = data[0];
+		dia = data[1];
+		ano = data[2];
+		
+	}
+	
+	public String retornaDataExtenso() {
+		
+		String data = "";
+		
+		if(mes == 1) {
+			
+			data += "Janeiro";
+			
+		}else if(mes == 2) {
+			
+			data += "Fevereiro";
+			
+		}else if(mes == 3) {
+			
+			data += "Março";
+			
+		}else if(mes == 4) {
+			
+			data += "Abril";
+			
+		}else if(mes == 5) {
+			
+			data += "Maio";
+			
+		}else if(mes == 6) {
+			
+			data += "Junho";
+			
+		}else if(mes == 7) {
+			
+			data += "Julho";
+			
+		}else if(mes == 8) {
+			
+			data += "Agosto";
+			
+		}else if(mes == 9) {
+			
+			data += "Setembro";
+			
+		}else if(mes == 10) {
+			
+			data += "Outubro";
+			
+		}else if(mes == 11) {
+			
+			data += "Novembro";
+			
+		}else if(mes == 12) {
+			
+			data += "Dezembro";
+			
+		}
+		
+		data += String.format(" %d, %d", dia, ano);
+		
+		return data;
+		
+	}
+	
+}
